220a73c1ee759015407f6e970b029b7d62f87928
JyunpeiKuroda <totoro.black6996@gmail.com>
2020-03-10 20:21:47 +0900

    ValiDemo-create
    ・新規投稿のバリデーション
    ・上に必要な(vue・Controller・Route)の作成。

---
+++ sample_laravel_app/app/Http/Controllers/ValiDemoController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ValiDemoController extends Controller {
+    // 表示画面を作成
+    public function getIndex() {
+        return view('validation.index');
+    }
+
+    public function confirm(Request $request) {
+        // バリデーションのルールを作成
+        $validateRules = [
+            'username'=>'required',
+            'mail'=>'required|email',
+            'age'=>'required|numeric',
+            'opinion'=>'required|max:500'
+        ];
+
+        // バリデーションのエラーメッセージを作成
+        $validateMessages = [
+            "required" => "必須項目です。",
+            "email" => "メールアドレスの形式で入力してください。",
+            "numeric" => "数値で入力してください。",
+            "opinion.max" => "500文字以内で入力してください。"
+        ];
+
+        //バリデーションをインスタンス化
+        $this->validate($request, $validateRules, $validateMessages);
+        $data = $request->all();
+        return view('validation.confirm')->with($data);
+
+    }
+}
