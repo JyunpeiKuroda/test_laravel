a0fdc28af42053a3aa4f898c248415b061ab0934
JyunpeiKuroda <totoro.black6996@gmail.com>
2020-03-22 20:32:33 +0900

    2020-0322-20:32

--- sample_laravel_app/resources/views/img/image_index.blade.php
+++ sample_laravel_app/resources/views/img/image_index.blade.php
@@ -2,25 +2,25 @@
 @section('title', '一覧画面')
 @section('content')
 
-@if (session('status'))
+{{-- @if (session('status'))
 <div class="alert alert-success" role="alert" onclick="this.classList.add('hidden')">{{ session('status') }}</div>
-@endif
+@endif --}}
 
-{!! Form::open(['url' => 'photos/store', 'files' => true,'method' => 'post']) !!}
+{{-- {!! Form::open(['url' => 'photos/store', 'files' => true,'method' => 'post']) !!}
 {!! Form::label('fileName', 'アップロード') !!}
 {!! Form::file('fileName') !!}
 {!! Form::submit('アップロードする') !!}
-{!! Form::close() !!}
+{!! Form::close() !!} --}}
 
 <h2>投稿一覧画面(image_index)</h2>
 <hr color="#000000" style="height:1px;">
 
-@foreach ($photos ?? '' as $photo)
+{{-- @foreach ($photos ?? '' as $photo) --}}
 <div class="panel panel-default">
-    <div class="panel-heading">アップロードした日付：{{$photo->created_at}}</div>
+    {{-- <div class="panel-heading">アップロードした日付：{{$photo->created_at}}</div> --}}
     <!-- List group -->
     <ul class="list-group">
-    <li class="list-group-item"><img src="{{$photo->path}}"></li>
+    {{-- <li class="list-group-item"><img src="{{$photo->path}}"></li> --}}
     </ul>
 </div>
 {{-- @endforeach --}}
